ff91be2e1d4bb28f2fb567cc9ea30a520aa0f7f3
sakawatari <under.fild1930@gmail.com>
2016-12-14 06:07:32 +0000

    gem-redcarpetを追加（おすすめ投稿でマークダウン形式に対応）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def markdown(text)
+    unless @markdown
+      renderer = Redcarpet::Render::HTML.new
+      renderer = Redcarpet::Render::HTML.new(filter_html: true)
+      renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+      @markdown = Redcarpet::Markdown.new(renderer, tables: true)
+      @markdown = Redcarpet::Markdown.new(renderer)
+    end
+
+    @markdown.render(text).html_safe
+  end
 end
