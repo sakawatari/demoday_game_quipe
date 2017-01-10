97276c478e2c9ae25be8dda8378360416b399505
sakawatari <under.fild1930@gmail.com>
2017-01-10 03:25:44 +0000

    ストック機能の追加

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,11 +3,13 @@ class StocksController < ApplicationController
     @stock = Stock.create(user_id: current_user.id, game_id: params[:game_id])
     @stocks = Stock.where(game_id: params[:game_id])
     @games = Game.all
+    redirect_to game_path(params[:game_id])
   end
   def destroy
     stock = Stock.find_by(user_id: current_user.id, game_id: params[:game_id])
     stock.destroy
     @stocks = Stock.where(game_id: params[:game_id])
     @games = Game.all
+    redirect_to game_path(params[:game_id])
   end
 end
